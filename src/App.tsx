9a7263306240cdb60eb5aaf0b486bf073f06aeb7
Mustafa Çor <corxjs@gmail.com>
2022-09-21 00:02:03 +0300

    poff

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import About from "./components/About";
 import MainLayout from "./layouts/MainLayout";
-
+import React, { ReactElement, ReactNode, useLayoutEffect, useState } from "react";
+import anime from "animejs";
+import Loading from "./components/Loading";
 export default function App() {
+
+  let [isLoading, setIsLoading] = useState(true);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <MainLayout>
-      <About/>
+      <About />
+      {isLoading && <Loading />}
     </MainLayout>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,44 @@ module.exports = {
         gold: "rgba(233, 220, 107, 1)",
         lightBlueForBoxShadow: "rgba(163, 243, 249, 0.25)",
         green: "rgba(45, 192, 157, 1)",
+      },
+      keyframes: {
+        go: {
+          '0%': { opacity:1 },
+          '50%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        triangleDraw: {
+          '0%': { 
+            strokeDashoffset: 1000,
+            strokeDasharray: 1000,
+          },
+          '100%': {
+            strokeDashoffset: 0,
+            strokeDasharray: 1000,
+            fill: "#B2FF9E",
+          },
+        },
+        triangleRotate: {
+          '0%': {
+            left: "-100%",
+            transform: "rotate(0deg)",
+          },
+          '90%':{
+            transform: "translateY(0px)",
+            transform: "rotate(350deg)",
+          },
+          '100%': {
+            left: "30%",
+            transform: "rotate(360deg) translateY(-100px)",
+
+          },
+        }
+      },
+      animation:{
+        go: 'go 3s ease',
+        triangleDraw: 'triangleDraw 5s ease forwards 4s',
+        triangleRotate: 'triangleRotate 6s ease forwards',
       }
     },
     fontFamily:{
